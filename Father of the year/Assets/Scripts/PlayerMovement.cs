2d673b498c014fe06cb1e661acf2f197a820d9d4
Tyson Hyland <tyhyland@hotmail.com>
2019-10-03 20:42:57 -0600

    Added player movement
    Horizontal movement and jump

---
+++ Father of the year/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D playerBody;
+    public float playerSpeed;
+    public float jumpSpeed;
+
+    void Awake()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); // left is -1, stopped is 0, right is 1
+
+        Vector2 movementPlayer = new Vector2(moveHorizontal, 0);
+        Vector2 jump = new Vector2(0, 50);
+
+        if (moveHorizontal > 0f)
+        {
+            playerBody.AddForce(movementPlayer * playerSpeed);
+        }
+        if (moveHorizontal < 0f)
+        {
+            playerBody.AddForce(movementPlayer * playerSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerBody.AddForce(jump*jumpSpeed);
+        }
+        Debug.Log(moveHorizontal);
+    }
+
+}
