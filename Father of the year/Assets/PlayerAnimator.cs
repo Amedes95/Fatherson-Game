72e03c378a314c4e1f1e45eb947dae087db8db84
Tyson Hyland <tyhyland@hotmail.com>
2019-10-03 21:51:37 -0600

    Jump detectors
    jump detectors to paint under floor tiles. player can only jump when on a jump detector

---
+++ Father of the year/Assets/PlayerAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator playerAnim;
+
+    void Start()
+    {
+        playerAnim = gameObject.GetComponent<Animator>();   
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
--- Father of the year/Assets/Scripts/PlayerMovement.cs
+++ Father of the year/Assets/Scripts/PlayerMovement.cs
@@ -8,17 +8,19 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D playerBody;
     public float playerSpeed;
     public float jumpSpeed;
+    private float fallVelocity;
 
     void Awake()
     {
         playerBody = GetComponent<Rigidbody2D>();
 
-    }
+}
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal"); // left is -1, stopped is 0, right is 1
+        fallVelocity = playerBody.velocity.y;
 
         Vector2 movementPlayer = new Vector2(moveHorizontal, 0);
         Vector2 jump = new Vector2(0, 50);
@@ -32,7 +34,7 @@ public class PlayerMovement : MonoBehaviour
             playerBody.AddForce(movementPlayer * playerSpeed);
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && JumpDetector.OnGround)
         {
             playerBody.AddForce(jump*jumpSpeed);
         }
