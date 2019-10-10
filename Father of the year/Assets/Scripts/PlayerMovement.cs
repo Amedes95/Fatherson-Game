8ee0eb7dd7e1eb38f111c212565d0752cd23d366
Amedes95 <pielord95@gmail.com>
2019-10-10 16:12:23 -0600

    The player now experiences death
    Added a basic HP script to the player, fixed the spike tiles (I changed it to 16 pixels per unit), and death canvas with respawn button.

--- Father of the year/Assets/Scripts/PlayerMovement.cs
+++ Father of the year/Assets/Scripts/PlayerMovement.cs
@@ -13,10 +13,13 @@ public class PlayerMovement : MonoBehaviour
     Vector2 antiGravity = new Vector2(0, 1);
     public float jumpHeight;
 
+    public Transform PlayerSpawn; // passed in through editor
+
     void Awake()
     {
         playerBody = GetComponent<Rigidbody2D>();
         PlayerAnim = GetComponent<Animator>();
+        transform.position = PlayerSpawn.position; // spawn
 
     }
 
@@ -28,55 +31,59 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal"); // left is -1, stopped is 0, right is 1
-        fallVelocity = playerBody.velocity.y;
-        jumpForce = CalculateJumpForce(playerBody.gravityScale, jumpHeight);
+        if (PlayerHealth.Dead == false) // Only allow movement if alive
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal"); // left is -1, stopped is 0, right is 1
+            fallVelocity = playerBody.velocity.y;
+            jumpForce = CalculateJumpForce(playerBody.gravityScale, jumpHeight);
 
-        Vector2 movementPlayer = new Vector2(moveHorizontal, 0);
+            Vector2 movementPlayer = new Vector2(moveHorizontal, 0);
 
-        if (Mathf.Abs(playerBody.velocity.x) > 10 && JumpDetector.OnGround)
-        {
-            playerSpeed = 20.75f;
-        }
-        else if (Mathf.Abs(playerBody.velocity.x) < 3 && JumpDetector.OnGround)
-        {
-            playerSpeed = 40;
-        }
-        else if (JumpDetector.OnGround)
-        {
-            playerSpeed = 21.5f;
-        }
+            if (Mathf.Abs(playerBody.velocity.x) > 10 && JumpDetector.OnGround)
+            {
+                playerSpeed = 20.75f;
+            }
+            else if (Mathf.Abs(playerBody.velocity.x) < 3 && JumpDetector.OnGround)
+            {
+                playerSpeed = 40;
+            }
+            else if (JumpDetector.OnGround)
+            {
+                playerSpeed = 21.5f;
+            }
 
-        if (Mathf.Abs(playerBody.velocity.x) > 10 && !JumpDetector.OnGround)
-        {
-            playerBody.AddForce(movementPlayer * playerSpeed*-1);
-        }
+            if (Mathf.Abs(playerBody.velocity.x) > 10 && !JumpDetector.OnGround)
+            {
+                playerBody.AddForce(movementPlayer * playerSpeed * -1);
+            }
 
 
-        if (fallVelocity < -10)
-        {
-            playerBody.AddForce(antiGravity * playerBody.gravityScale * 10);
-        }
+            if (fallVelocity < -10)
+            {
+                playerBody.AddForce(antiGravity * playerBody.gravityScale * 10);
+            }
 
-        ///// RUNNING
-        if (moveHorizontal > 0f) // player is moving right
-        {
-            playerBody.AddForce(movementPlayer * playerSpeed);
-            PlayerAnim.SetBool("Running", true);
-            transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
-        }
-        if (moveHorizontal < 0f) // player is moving left
-        {
-            playerBody.AddForce(movementPlayer * playerSpeed);
-            PlayerAnim.SetBool("Running", true);
-            transform.localScale = new Vector2(-Mathf.Abs(transform.localScale.x), transform.localScale.y);
-        }
-        if (Mathf.Abs(moveHorizontal) < .01) // player is stopped
-        {
-            PlayerAnim.SetBool("Running", false);
-        }
+            ///// RUNNING
+            if (moveHorizontal > 0f) // player is moving right
+            {
+                playerBody.AddForce(movementPlayer * playerSpeed);
+                PlayerAnim.SetBool("Running", true);
+                transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+            }
+            if (moveHorizontal < 0f) // player is moving left
+            {
+                playerBody.AddForce(movementPlayer * playerSpeed);
+                PlayerAnim.SetBool("Running", true);
+                transform.localScale = new Vector2(-Mathf.Abs(transform.localScale.x), transform.localScale.y);
+            }
+            if (Mathf.Abs(moveHorizontal) < .01) // player is stopped
+            {
+                PlayerAnim.SetBool("Running", false);
+            }
 
-        Debug.Log(playerBody.velocity.x);
+            Debug.Log(playerBody.velocity.x);
+        }
+        
     }
 
     public void Jump()
@@ -88,10 +95,20 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        //// JUMPING
-        if (Input.GetKeyDown(KeyCode.W) && JumpDetector.OnGround) // checks to see if player is on ground
+        if (PlayerHealth.Dead == false) // Only allow inputs if alive
         {
-            Jump();
+            //// JUMPING
+            if (Input.GetKeyDown(KeyCode.W) && JumpDetector.OnGround) // checks to see if player is on ground
+            {
+                Jump();
+            }
         }
+
+    }
+
+    public void Respawn()
+    {
+        //moves the player to SpawnPosition
+        transform.position = PlayerSpawn.position;
     }
 }
--- Father of the year/Assets/Scripts/SpikeDetector.cs
+++ Father of the year/Assets/Scripts/SpikeDetector.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class SpikeDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    // This function gets called when the player's collider touches the spike collider
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player") // make sure it's the player
+        {
+            collision.GetComponent<PlayerHealth>().KillPlayer(); // I reference the HP component on the player and call one of its functions
+        }
     }
 }
