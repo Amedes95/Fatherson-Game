dce6fbbd022e17a948b101347f44390ad269c1e5
Amedes95 <pielord95@gmail.com>
2019-10-11 16:11:05 -0600

    Revert "Jump zones are obsolete"
    This reverts commit f06fcafc661c344278e84febf82045ac22e3c044.

--- Father of the year/Assets/Scripts/JumpDetector.cs
+++ Father of the year/Assets/Scripts/JumpDetector.cs
@@ -7,23 +7,11 @@ public class JumpDetector : MonoBehaviour
 
     public static bool OnGround;
     public Transform EndLine;
-    public Transform EndLine2;
-    bool grounded2;
-
 
 
     private void Update()
     {
         Raycasting();
-        if (grounded2)
-        {
-            OnGround = true;
-        }
-        if (OnGround)
-        {
-            grounded2 = true;
-        }
-
         if (OnGround)
         {
             PlayerMovement.isJumping = false;
@@ -40,10 +28,7 @@ public class JumpDetector : MonoBehaviour
 
     void Raycasting()
     {
-        Debug.DrawLine(transform.position, EndLine.position, Color.green);
-        Debug.DrawLine(transform.position, EndLine2.position, Color.green);
-
-        OnGround = Physics2D.Linecast(transform.position, EndLine.position, 1 << LayerMask.NameToLayer("Ground"));
-        grounded2 = Physics2D.Linecast(transform.position, EndLine2.position, 1 << LayerMask.NameToLayer("Ground"));
+        Debug.DrawLine(transform.position, EndLine.position, Color.green); 
+        OnGround = Physics2D.Linecast(transform.position, EndLine.position, 1 << LayerMask.NameToLayer("JumpZone"));
     }
 }
