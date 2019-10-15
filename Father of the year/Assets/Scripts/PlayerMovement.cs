a010b1737afaca16cb53cc2bfed4a1c7ff07dc99
Tyson Hyland <tyhyland@hotmail.com>
2019-10-14 22:04:07 -0600

    Walljumping and Wallsliding

--- Father of the year/Assets/Scripts/PlayerMovement.cs
+++ Father of the year/Assets/Scripts/PlayerMovement.cs
@@ -23,6 +23,12 @@ public class PlayerMovement : MonoBehaviour
     public float maxVelocity;
     public float midVelocity;
     public static bool Sprinting;
+    public Transform wallEndLine;
+    bool touchingWall;
+    bool wallClinging;
+    int playerDirection;
+    Vector2 walljump;
+
     Transform PlayerSpawn; // passed in through editor
 
     void Awake()
@@ -36,7 +42,7 @@ public class PlayerMovement : MonoBehaviour
         fullSpeed = 14; //full speed should always be less than mid speed
         maxVelocity = 5;
         midVelocity = 3;
-        
+
 
     }
 
@@ -56,13 +62,19 @@ public class PlayerMovement : MonoBehaviour
 
             Vector2 movementPlayer = new Vector2(moveHorizontal, 0);
 
+            if (transform.localScale.x > 0)
+            { playerDirection = 1; }
+            else
+            { playerDirection = -1; }
+
+            walljump = new Vector2(-playerDirection, 1);
             if (playerBody.velocity.y < -1)
             {
-                playerBody.AddForce(new Vector2(0, -1)*fallForce);
+                playerBody.AddForce(new Vector2(0, -1) * fallForce);
             }
 
             ///// Sprinting
-             if (Sprinting)
+            if (Sprinting && JumpDetector.OnGround)
             {
                 PlayerAnim.SetFloat("SprintSpeed", 2);
                 startSpeed = 200;
@@ -72,7 +84,7 @@ public class PlayerMovement : MonoBehaviour
                 midVelocity = 5;
             }
 
-             if (!Sprinting)
+            if (!Sprinting && !JumpDetector.OnGround)
             {
                 PlayerAnim.SetFloat("SprintSpeed", 1);
                 startSpeed = 60;
@@ -80,6 +92,15 @@ public class PlayerMovement : MonoBehaviour
                 fullSpeed = 14;
                 maxVelocity = 4;
                 midVelocity = 3;
+            }
+
+            if (touchingWall && !JumpDetector.OnGround)
+            {
+                if (playerBody.velocity.y < -.5f)
+                {
+                    isJumping = false;
+                    playerBody.velocity = new Vector2(playerBody.velocity.x, -.5f);
+                }
             }
 
 
@@ -114,14 +135,13 @@ public class PlayerMovement : MonoBehaviour
             {
                 playerBody.AddForce(movementPlayer * playerSpeed);
                 PlayerAnim.SetBool("Running", true);
-                transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
             }
             if (moveHorizontal < 0f) // player is moving left
             {
                 playerBody.AddForce(movementPlayer * playerSpeed);
                 PlayerAnim.SetBool("Running", true);
-                transform.localScale = new Vector2(-Mathf.Abs(transform.localScale.x), transform.localScale.y);
             }
+
             if (Mathf.Abs(moveHorizontal) < .01) // player is stopped
             {
                 PlayerAnim.SetBool("Running", false);
@@ -135,15 +155,26 @@ public class PlayerMovement : MonoBehaviour
                 }
             }
         }
-        Debug.Log(playerBody.velocity.x);
     }
 
     public void Jump()
     {
         Vector2 jump = Vector2.up;
-        playerBody.AddForce(jump * jumpForce *  180);
+        playerBody.AddForce(jump * jumpForce * 180);
         PlayerAnim.SetTrigger("Jump");
         isJumping = true;
+    }
+
+    public void WallJump()
+    {
+        playerBody.AddForce(walljump * jumpForce * 180);
+        isJumping = true;
+    }
+
+    public void WallRaycasting()
+    {
+        Debug.DrawLine(transform.position, wallEndLine.position, Color.green);  // during playtime, projects a line from a start point to and end point
+        touchingWall = Physics2D.Linecast(transform.position, wallEndLine.position, 1 << LayerMask.NameToLayer("Ground"));
     }
 
     private void Update()
@@ -155,7 +186,12 @@ public class PlayerMovement : MonoBehaviour
             {
                 jumpKeyHeld = true;
 
-                if (!JumpDetector.OnGround && jumpCount > 0)
+                if (!JumpDetector.OnGround && touchingWall) //Walljump
+                {
+                    WallJump();
+                }
+
+                if (!JumpDetector.OnGround && jumpCount > 0 && !touchingWall) //Double jump
                 {
                     playerBody.velocity = new Vector2(playerBody.velocity.x, 0);
                     Jump();
@@ -165,7 +201,6 @@ public class PlayerMovement : MonoBehaviour
                 if (JumpDetector.OnGround) // Checks to see if player is on ground before jumping
                 {
                     Jump();
-                    //isJumping = true;
                 }
                 
             }
@@ -182,6 +217,22 @@ public class PlayerMovement : MonoBehaviour
             if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
             {
                 Sprinting = false;  
+            }
+
+            //// Walljumping
+            WallRaycasting();
+
+            ////Face direction of horizontal movement
+            if (playerBody.velocity.x > .5f)
+            {
+                transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+                Debug.Log("Flip");
+            }
+
+            if (playerBody.velocity.x < -.5f)
+            {
+                transform.localScale = new Vector2(-Mathf.Abs(transform.localScale.x), transform.localScale.y);
+                Debug.Log("Flip");
             }
         }
     }
