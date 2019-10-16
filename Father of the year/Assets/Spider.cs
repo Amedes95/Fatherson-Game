f13ec9866fca5dbb20b386d5f5f88361f887dba7
Amedes95 <pielord95@gmail.com>
2019-10-15 20:59:52 -0600

    Added another spider

--- Father of the year/Assets/Spider.cs
+++ Father of the year/Assets/Spider.cs
@@ -6,12 +6,32 @@ public class Spider : MonoBehaviour
 {
 
     Animator SpiderAnim;
+    Vector2 JumpAngle;
+    public float JumpSpeed;
+    float Direction;
+    public Transform EndLine;
+    bool TouchingFloor;
+
+
     // Start is called before the first frame update
     void Awake()
     {
         SpiderAnim = gameObject.GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        Direction = Mathf.Sign(transform.localScale.x);
+        RaycastingFloor();
+        if (TouchingFloor == false)
+        {
+            SpiderAnim.SetBool("InAir", true);
+        }
+        else
+        {
+            SpiderAnim.SetBool("InAir", false);
+        }
+    }
 
     public void DecideLength()
     {
@@ -29,5 +49,23 @@ public class Spider : MonoBehaviour
         {
             SpiderAnim.SetTrigger("Long");
         }
+    }
+
+    public void Hop() // called during the last frame of the idle animation
+    {
+        if (TouchingFloor == true)
+        {
+            Debug.Log("Hippty Hoppity");
+            JumpAngle = new Vector2(Direction / 2, Mathf.Sqrt(3) / 2);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(JumpAngle * JumpSpeed);
+            SpiderAnim.SetTrigger("Hop");
+        }
+
+    }
+
+    void RaycastingFloor()
+    {
+        Debug.DrawLine(transform.position, EndLine.position, Color.green);  // during playtime, projects a line from a start point to and end point
+        TouchingFloor = Physics2D.Linecast(transform.position, EndLine.position, 1 << LayerMask.NameToLayer("Ground")); // returns true if line touches a ground tile
     }
 }
