dcc9b9072d541dcd7742846e44a0d41cb88af146
Amedes95 <pielord95@gmail.com>
2019-10-17 01:25:16 -0600

    Vanishing Blocks Prefab
    When touched, vanish slowly.  Do not respawn.

---
+++ Father of the year/Assets/Crumble.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crumble : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Feet" || collision.tag == "Player")
+        {
+            GetComponent<Animator>().SetTrigger("Crumble");
+        }
+    }
+
+    public void DisableMe()
+    {
+        gameObject.SetActive(false);
+    }
+}
