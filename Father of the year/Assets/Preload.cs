45634682bce07610e29c92b1cca0a57c6793943b
Amedes95 <pielord95@gmail.com>
2019-10-16 23:59:17 -0600

    Level preload screen
    Work in progress

---
+++ Father of the year/Assets/Preload.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Preload : MonoBehaviour
+{
+    public string NameToDisplay;
+
+    private void Awake()
+    {
+        GetComponentInChildren<Text>().text = NameToDisplay;
+    }
+}
