6b385bfdf83de527f9980be0f6e1636435f83fda
Amedes95 <pielord95@gmail.com>
2019-10-17 16:09:57 -0600

    Vanishing block prefabs now respawn if toggled
    Also deleted the long versions.  Blocks will not respawn if the player is blocking the box collider

--- Father of the year/Assets/Crumble.cs
+++ Father of the year/Assets/Crumble.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class Crumble : MonoBehaviour
 {
+    public GameObject CrumbleChild;
+    public bool Respawnable;
+    public float SpawnDelay; // how long it is disabled for
 
+    bool Disabled;
+    float SpawnDelayReset;
+    bool BlockingRespawn;
+
+    private void Awake()
+    {
+        SpawnDelayReset = SpawnDelay;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,8 +25,45 @@ public class Crumble : MonoBehaviour
         }
     }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Feet" || collision.tag == "Player")
+        {
+            BlockingRespawn = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Feet" || collision.tag == "Player")
+        {
+            BlockingRespawn = false;
+        }
+    }
+
+
     public void DisableMe()
     {
-        gameObject.SetActive(false);
+        CrumbleChild.SetActive(false);
+        Disabled = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Respawnable) // only do this if I'm able to respawn
+        {
+            if (Disabled && !BlockingRespawn)
+            {
+                SpawnDelay -= Time.smoothDeltaTime;
+                if (SpawnDelay <= 0)
+                {
+                    SpawnDelay = 0; // no negatives pls
+                    Disabled = false;
+                    SpawnDelay = SpawnDelayReset; // put it back time to the original start value
+                    CrumbleChild.SetActive(true);
+                    GetComponent<Animator>().SetTrigger("Respawn");
+                }
+            }
+        }
     }
 }
