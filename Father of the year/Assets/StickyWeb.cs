56e3dfc92ff6dda4ff91904292a40a48ba5effe9
Amedes95 <pielord95@gmail.com>
2019-10-17 22:08:17 -0600

    Sticky web added
    Not fully functional yet

---
+++ Father of the year/Assets/StickyWeb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyWeb : MonoBehaviour
+{
+    public static bool StuckInWeb;
+
+
+    private void OnTriggerStay2D(Collider2D collision) // enter the web
+    {
+        if (collision.tag == "Player")
+        {
+            StuckInWeb = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) // leave the web
+    {
+        if (collision.tag == "Player")
+        {
+            StuckInWeb = false;
+        }
+    }
+}
