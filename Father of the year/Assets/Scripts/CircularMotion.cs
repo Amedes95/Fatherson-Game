ca69ce2e59d8b1e8a59cc5e3a0e64858299724d8
Tyson Hyland <tyhyland@hotmail.com>
2019-10-17 19:21:48 -0600

    added circular motion script

---
+++ Father of the year/Assets/Scripts/CircularMotion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMotion : MonoBehaviour
+{
+    public float timeCounter;
+    public Vector3 initialPosition; // copy and paste the starting position from parent transform
+    public float radius;
+    public float speed;
+    public bool clockwise;
+    public int direction;
+    float x;
+    float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 0);
+        if (clockwise)
+        {
+            direction = -1;
+        }
+        else
+        {
+            direction = 1;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.smoothDeltaTime;
+        x = radius * Mathf.Cos(timeCounter * speed * 2 * Mathf.PI * direction);
+        y = radius * Mathf.Sin(timeCounter * speed * 2 * Mathf.PI * direction);
+
+        transform.position =  initialPosition + new Vector3(x, y, 0);
+    }
+}
