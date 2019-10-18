3d08f2e0d7358b951559a8ec5fc7a602ee8957c5
Amedes95 <pielord95@gmail.com>
2019-10-18 00:59:15 -0600

    Pause Menu g2g
    Transitions work

--- Father of the year/Assets/Scripts/PauseMenu.cs
+++ Father of the year/Assets/Scripts/PauseMenu.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.PostProcessing;
+
 
 public class PauseMenu : MonoBehaviour
 {
@@ -9,19 +11,32 @@ public class PauseMenu : MonoBehaviour
     public GameObject PauseScreen;
     VictoryMenu VictoryScreen;
     GameObject Player;
+    GameObject DeathScreen;
+
+    public PostProcessingProfile Transition1; // For film grain and black wipe effects
+    bool transitioning;
+    public float ShadowValueUp;
 
 
     // Start is called before the first frame update
     void Awake()
     {
+        transitioning = false;
         GameIsPaused = false;
+        DeathScreen = GameObject.FindGameObjectWithTag("DeathMenu");
         VictoryScreen = GameObject.FindGameObjectWithTag("VictoryMenu").GetComponent<VictoryMenu>();
         Player = GameObject.FindGameObjectWithTag("Player");
+        var Blurry = Transition1.depthOfField.settings;
+        Blurry.focalLength = 0f;
+        Transition1.depthOfField.settings = Blurry;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        var Vinny = Transition1.vignette.settings; // black wipe effect
+
         if (Input.GetKeyDown(KeyCode.Escape) && VictoryScreen.LevelComplete == false && Player.activeInHierarchy)
         {
             if (GameIsPaused)
@@ -33,6 +48,24 @@ public class PauseMenu : MonoBehaviour
                 PauseGame();
             }
         }
+
+        if (transitioning)
+        {
+            Vinny.intensity += ShadowValueUp;
+            if (Vinny.intensity >= 1)
+            {
+                SceneManager.LoadScene("WorldHub");
+            }
+        }
+        else
+        {
+            Vinny.intensity -= (ShadowValueUp + .01f);
+            if (Vinny.intensity <= 0)
+            {
+                Vinny.intensity = 0;
+            }
+        }
+        Transition1.vignette.settings = Vinny;
     }
 
     public void ResumeGame()
@@ -40,6 +73,11 @@ public class PauseMenu : MonoBehaviour
         PauseScreen.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        DeathScreen.SetActive(true);
+        VictoryScreen.gameObject.SetActive(true);
+        var Blurry = Transition1.depthOfField.settings;
+        Blurry.focalLength = 0f;
+        Transition1.depthOfField.settings = Blurry;
 
     }
     public void PauseGame()
@@ -49,6 +87,11 @@ public class PauseMenu : MonoBehaviour
             PauseScreen.SetActive(true);
             Time.timeScale = 0f;
             GameIsPaused = true;
+            DeathScreen.SetActive(false);
+            VictoryScreen.gameObject.SetActive(false);
+            var Blurry = Transition1.depthOfField.settings;
+            Blurry.focalLength = 300f;
+            Transition1.depthOfField.settings = Blurry;
         }
     }
 
@@ -61,6 +104,10 @@ public class PauseMenu : MonoBehaviour
     public void QuitLevel()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("MainMenu");
+        transitioning = true;
+        DeathScreen.SetActive(false);
+        VictoryScreen.gameObject.SetActive(false);
+        PauseScreen.SetActive(false);
+
     }
 }
