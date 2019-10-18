fbc5e199f7f839e07fea46778429745ec54cd11f
Tyson Hyland <tyhyland@hotmail.com>
2019-10-18 01:01:25 -0600

    camera bounds bool added

--- Father of the year/Assets/Scripts/Camera Scripts/CameraFollower.cs
+++ Father of the year/Assets/Scripts/Camera Scripts/CameraFollower.cs
@@ -8,6 +8,9 @@ public class CameraFollower : MonoBehaviour
     float CameraSpeed;
     public float maxCameraSpeed;
     Vector3 FocusZoneFix;
+    public bool cameraBounds;
+    public Vector3 minCameraBounds;
+    public Vector3 maxCameraBounds;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +24,13 @@ public class CameraFollower : MonoBehaviour
         CameraSpeed = Mathf.Min(PlayerMovement.playerVelocity / PlayerMovement.maxVelocity * maxCameraSpeed + .05f, maxCameraSpeed);
         FocusZoneFix = new Vector3(FocusZone.transform.position.x, FocusZone.transform.position.y, -10f);
         transform.position = Vector3.MoveTowards(transform.position, FocusZoneFix, CameraSpeed);
+
+        if (cameraBounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraBounds.x, maxCameraBounds.x),
+                Mathf.Clamp(transform.position.y, minCameraBounds.y, maxCameraBounds.y),
+                Mathf.Clamp(transform.position.z, minCameraBounds.z, maxCameraBounds.z)
+                );
+        }
     }
 }
