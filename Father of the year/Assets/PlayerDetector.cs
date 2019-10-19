a4112167d29e96bc38fb1fd97bb263267382c60a
Amedes95 <pielord95@gmail.com>
2019-10-18 18:40:06 -0600

    Cobra spaz bug and dropping platforms

---
+++ Father of the year/Assets/PlayerDetector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    public bool PlayerInRange;
+
+    /// used with hopping spider
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayerInRange = false;
+        }
+    }
+}
