d452c02c8b2fa0a0e63549131d2f0cb40a9f4638
Amedes95 <pielord95@gmail.com>
2019-10-24 22:31:13 -0600

    Fruits now respawn
    5 second delay.  you can edit this

--- Father of the year/Assets/Scripts/Collectible.cs
+++ Father of the year/Assets/Scripts/Collectible.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     public int Value; // How nutritious is this fruit?
+    public bool Respawnable;
+    public float RespawnDelay;
+    float RespawnDelayStart;
+    bool Consumed;
+
+    private void Awake()
+    {
+        RespawnDelayStart = RespawnDelay;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision) // Triggered when fruit is picked up by the player
     {
@@ -13,6 +22,21 @@ public class Collectible : MonoBehaviour
             GetComponent<Animator>().SetTrigger("Collected"); // pop
             collision.GetComponent<Collector>().FruitFromLevel += Value; // The players current fruit plus the value of the fruit obtained
             PlayerMovement.jumpCount = 1;
+            Consumed = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Consumed)
+        {
+            RespawnDelay -= Time.smoothDeltaTime;
+            if (RespawnDelay <= 0)
+            {
+                GetComponent<Animator>().SetTrigger("Respawn");
+                Consumed = false;
+                RespawnDelay = RespawnDelayStart;
+            }
         }
     }
 }
