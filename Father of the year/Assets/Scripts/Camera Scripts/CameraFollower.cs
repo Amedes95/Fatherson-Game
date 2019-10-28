679c4cce5c98b525e457559853810d97f8965870
Tyson Hyland <tyhyland@hotmail.com>
2019-10-28 13:59:49 -0600

    Added Max Distance From Player float to camera follower
    This makes a circle of radius float around the camera follower, and if the camera exits that cirlce it starts to focus on the player. 12 feels pretty good.

--- Father of the year/Assets/Scripts/Camera Scripts/CameraFollower.cs
+++ Father of the year/Assets/Scripts/Camera Scripts/CameraFollower.cs
@@ -7,6 +7,7 @@ public class CameraFollower : MonoBehaviour
     private GameObject FocusZone;
     float CameraSpeed;
     public float maxCameraSpeed;
+    public float maxDistanceFromPlayer;
     Vector3 FocusZoneFix;
     public bool cameraBounds;
     public Vector3 minCameraBounds;
@@ -26,7 +27,7 @@ public class CameraFollower : MonoBehaviour
         CameraSpeed = Mathf.Min(PlayerMovement.playerVelocity / PlayerMovement.maxVelocity * maxCameraSpeed + .05f, maxCameraSpeed); // This scales the speed the camera moves towards the focus zone according to player velocity.
         FocusZoneFix = new Vector3(FocusZone.transform.position.x, FocusZone.transform.position.y, -10f); // This has (x,y) coordinates of the focus zone with the proper z value (-10) for the camera. The camera bugs out if it's z position is not -10
 
-        if (Vector3.Magnitude(FocusZoneFix - transform.position) > 12 || PlayerMovement.playerVelocity > 9)
+        if (Vector3.Magnitude(FocusZoneFix - transform.position) > maxDistanceFromPlayer || PlayerMovement.playerVelocity > 9)
         {
             cameraMoving = true;
         }
