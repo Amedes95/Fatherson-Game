a5351ba354955cd711427c9c7e168abacafe6060
Tyson Hyland <tyhyland@hotmail.com>
2019-10-28 22:52:53 -0600

    Update CameraFollower.cs

--- Father of the year/Assets/Scripts/Camera Scripts/CameraFollower.cs
+++ Father of the year/Assets/Scripts/Camera Scripts/CameraFollower.cs
@@ -35,7 +35,7 @@ public class CameraFollower : MonoBehaviour
         {
             cameraMoving = true;
         }
-        else if (Vector3.Magnitude(FocusZoneFix - transform.position) < .01f
+        else if (Vector3.Magnitude(FocusZoneFix - transform.position) < 1f
             //&& PlayerMovement.playerVelocity.magnitude < .01f
             )
         {
