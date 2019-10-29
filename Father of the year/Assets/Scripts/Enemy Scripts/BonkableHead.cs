68e3d7114c9192c2727d732c8e4ce4a1d24260f7
Tyson Hyland <tyhyland@hotmail.com>
2019-10-29 12:44:06 -0600

    removed trampoline logic from bonkable head

--- Father of the year/Assets/Scripts/Enemy Scripts/BonkableHead.cs
+++ Father of the year/Assets/Scripts/Enemy Scripts/BonkableHead.cs
@@ -8,24 +8,24 @@ public class BonkableHead : MonoBehaviour
     bool isBonking;
     public bool Killable;
     bool isEnemy;
-    public float rotation; //only use this on springs, not enemies
-    Vector2 rotationVector;
+    //public float rotation; //only use this on springs, not enemies
+    //Vector2 rotationVector;
 
 
     public void Awake()
     {
         isBonking = false;
-        gameObject.transform.parent.transform.eulerAngles = new Vector3 (0, 0, rotation);
-        rotationVector = new Vector2(Mathf.Cos(rotation), Mathf.Sin(rotation));
+        //gameObject.transform.parent.transform.eulerAngles = new Vector3 (0, 0, rotation);
+        //rotationVector = new Vector2(Mathf.Cos(rotation), Mathf.Sin(rotation));
 
-        if (gameObject.transform.parent.tag == "Enemy" || gameObject.transform.parent.tag == "Sleeper")
-        {
-            isEnemy = true;
-        }
-        else
-        {
-            isEnemy = false;
-        }
+        //if (gameObject.transform.parent.tag == "Enemy" || gameObject.transform.parent.tag == "Sleeper")
+        //{
+        //    isEnemy = true;
+        //}
+        //else
+        //{
+        //    isEnemy = false;
+        //}
 
     }
 
@@ -36,19 +36,19 @@ public class BonkableHead : MonoBehaviour
 
             if (!PlayerHealth.Dead && !isBonking)
             {
-                if (!isEnemy)
-                {
+                //if (!isEnemy)
+                //{
                     BonkForce = PlayerMovement.jumpForce;
-                }
-                else
-                {
-                    BonkForce = PlayerMovement.jumpForce / 2;
-                }
+                //}
+                //else
+                //{
+                //    BonkForce = PlayerMovement.jumpForce / 2;
+                //}
                 collision.GetComponentInParent<Rigidbody2D>().velocity =
                     new Vector2(collision.GetComponentInParent<Rigidbody2D>().velocity.x, 0);
-                collision.GetComponentInParent<Rigidbody2D>().AddForce(rotationVector * BonkForce * 180);
+                //collision.GetComponentInParent<Rigidbody2D>().AddForce(rotationVector * BonkForce * 180);
                 isBonking = true;
-                //collision.GetComponentInParent<Rigidbody2D>().AddForce(Vector2.up * PlayerMovement.jumpForce * 180);
+                collision.GetComponentInParent<Rigidbody2D>().AddForce(Vector2.up * PlayerMovement.jumpForce * 180);
                 if (gameObject.tag == "Trampoline")
                 {
                     GetComponent<Animator>().SetTrigger("Bounce");
@@ -65,7 +65,7 @@ public class BonkableHead : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Feet")
-        {
+        { 
             isBonking = false;
         }
     }
