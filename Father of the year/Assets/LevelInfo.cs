dd5cee172676b03a99e88fb10d5385939ab9ff36
Amedes95 <pielord95@gmail.com>
2019-10-30 15:11:37 -0600

    Level select screen edits

--- Father of the year/Assets/LevelInfo.cs
+++ Father of the year/Assets/LevelInfo.cs
@@ -7,5 +7,31 @@ public class LevelInfo : MonoBehaviour
     public string LevelDisplayName;
     public string WorldDisplayName;
     public string SceneToLoad;
+    public GameObject CompleteSymbol;
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Active");
+        gameObject.GetComponent<Animator>().SetBool("Active", true);
+
+        //// for completion
+        if (PlayerPrefs.GetInt(SceneToLoad) == 1) // 1 true, 0 false
+        {
+            CompleteSymbol.SetActive(true);
+        }
+        else
+        {
+            CompleteSymbol.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        gameObject.GetComponent<Animator>().SetBool("Active", false);
+    }
+
+    private void Update()
+    {
+
+    }
 }
