019009351ae2c1c3b6274c6ac999a720e02ef5c6
Amedes95 <pielord95@gmail.com>
2019-11-16 11:51:00 -0700

    Music stuff

---
+++ Father of the year/Assets/BackgroundMusic.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public static BackgroundMusic control;
+
+    public static AudioSource GameMusicPlayer;
+    public AudioClip TutorialMusic;
+    public AudioClip MenuMusic;
+    public AudioClip WorldHubMusic;
+
+    public AudioClip CurrentMusic;
+
+    AudioClip LevelMusic;
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameMusicPlayer = gameObject.GetComponent<AudioSource>();
+        LevelMusic = GameObject.FindGameObjectWithTag("LevelBoombox").GetComponent<Boombox>().LevelMusic;
+
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+
+        GameMusicPlayer.clip = CurrentMusic;
+        GameMusicPlayer.Play();
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if (CurrentMusic != LevelMusic)
+        {
+            GameMusicPlayer.clip = LevelMusic;
+            GameMusicPlayer.Play();
+        }
+    }
+
+}
---
+++ Father of the year/Assets/Boombox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boombox : MonoBehaviour
+{
+    public AudioClip LevelMusic;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //if (BackgroundMusic.control.CurrentMusic  != LevelMusic)
+        //{
+        //    BackgroundMusic.control.CurrentMusic = LevelMusic;
+        //}
+    }
+}
