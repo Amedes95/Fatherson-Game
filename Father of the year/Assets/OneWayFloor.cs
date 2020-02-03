819194ccb81a4413df3e11afe393aaf16b82e160
Amedes95 <pielord95@gmail.com>
2020-02-02 18:59:07 -0700

    Drop platform fix and corner stuck bug

--- Father of the year/Assets/OneWayFloor.cs
+++ Father of the year/Assets/OneWayFloor.cs
@@ -8,67 +8,119 @@ public class OneWayFloor : MonoBehaviour
     float WaitTime;
     string DropInput;
     string JumpInput;
+    bool insideZone;
 
     // Start is called before the first frame update
     void Start()
     {
-        effector2D = GetComponent<PlatformEffector2D>();
+        effector2D = GetComponentInParent<PlatformEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+
         // first see if we are using a controller or not
         if (Boombox.ControllerModeEnabled)
         {
             DropInput = "DropController";
             JumpInput = "JumpController";
-            if (Input.GetAxis(DropInput) != -1) // not holding down
+            if (insideZone)
             {
-                WaitTime = .5f;
-            }
-            if (Input.GetAxis(DropInput) == -1) // holding down
-            {
-                if (WaitTime <= 0)
+
+                if (Input.GetAxis(DropInput) != 1)
                 {
-                    effector2D.rotationalOffset = 180;
-                    WaitTime = .5f;
+                    WaitTime = .1f;
                 }
-                else
+                if (Input.GetAxis(DropInput) == 1) // holding down
                 {
-                    WaitTime -= Time.deltaTime;
+                    if (WaitTime <= 0)
+                    {
+                        effector2D.rotationalOffset = 180;
+                        WaitTime = .1f;
+                    }
+                    else
+                    {
+                        WaitTime -= Time.deltaTime;
+                    }
+                }
+                if (Input.GetButton(JumpInput))
+                {
+                    effector2D.rotationalOffset = 0;
                 }
             }
-            if (Input.GetButton(JumpInput)) // jump is pressed
+            else
             {
                 effector2D.rotationalOffset = 0;
             }
-        }
-        else
-        {
-            DropInput = "Drop";
-            JumpInput = "Jump";
-            if (Input.GetButtonUp(DropInput)) // use buttons instead of joystick axis
+            if (Input.GetAxis(DropInput) != 1) // use buttons instead of joystick axis
             {
                 WaitTime = .1f;
-            }
-            if (Input.GetButton(DropInput))
-            {
-                if (WaitTime <= 0)
-                {
-                    effector2D.rotationalOffset = 180;
-                    WaitTime = .1f;
-                }
-                else
-                {
-                    WaitTime -= Time.deltaTime;
-                }
             }
             if (Input.GetButton(JumpInput))
             {
                 effector2D.rotationalOffset = 0;
             }
+
+        }
+        else
+        {
+            DropInput = "Drop";
+            JumpInput = "Jump";
+            if (insideZone)
+            {
+
+                if (Input.GetButtonUp(DropInput)) // use buttons instead of joystick axis
+                {
+                    WaitTime = .1f;
+                }
+                if (Input.GetButton(DropInput))
+                {
+                    if (WaitTime <= 0)
+                    {
+                        effector2D.rotationalOffset = 180;
+                        WaitTime = .1f;
+                    }
+                    else
+                    {
+                        WaitTime -= Time.deltaTime;
+                    }
+                }
+                if (Input.GetButton(JumpInput))
+                {
+                    effector2D.rotationalOffset = 0;
+                }
+            }
+            else
+            {
+                effector2D.rotationalOffset = 0;
+            }
+            if (Input.GetButtonUp(DropInput)) // use buttons instead of joystick axis
+            {
+                WaitTime = .1f;
+            }
+            if (Input.GetButton(JumpInput))
+            {
+                effector2D.rotationalOffset = 0;
+            }
+
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            insideZone = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            insideZone = false;
         }
     }
 }
