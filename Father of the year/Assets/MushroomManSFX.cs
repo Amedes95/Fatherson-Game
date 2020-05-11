bbb53081ee6b9cdc85312a244cd8429ca0bf0f60
Amedes95 <pielord95@gmail.com>
2020-05-10 21:29:32 -0600

    Additional effects to other bosses
    World 2 boss now shows that he's sleeping.
    World 3 boss loaded up with new music and SFX
    
    Prepare for a battle!

---
+++ Father of the year/Assets/MushroomManSFX.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomManSFX : MonoBehaviour
+{
+    public AudioClip Grunt1;
+    public AudioClip Grunt2;
+    public AudioClip KnockedDown;
+    public AudioClip Wounded;
+    AudioSource MushroomMan;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        MushroomMan = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayHurtSound()
+    {
+        if (Random.Range(0, 2) == 0) // is it 0?
+        {
+            MushroomMan.clip = Grunt1;
+            MushroomMan.Play();
+        }
+        else // must be a 1
+        {
+            MushroomMan.clip = Grunt2;
+            MushroomMan.Play();
+        }
+    }
+
+    public void PlayKNockedNoise()
+    {
+        MushroomMan.clip = KnockedDown;
+        MushroomMan.Play();
+    }
+
+    public void PlayWoundedNoise()
+    {
+        MushroomMan.clip = Wounded;
+        MushroomMan.Play();
+    }
+}
